ca9751fc5fabb01ff1df334e5dc76b7d747847bf
Quramy <yosuke.kurami@gmail.com>
2021-03-23 03:10:26 +0900

    refactor: Use readline module

---
+++ src/cli/index.ts
@@ -0,0 +1,63 @@
+import readline from "readline";
+import { parse } from "../parser";
+import { evaluate } from "../evaluate";
+import { createTypePrinter, getPrimaryType } from "../type-checker";
+import { color } from "./color";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.setPrompt(color.green("> "));
+rl.prompt();
+
+let buf: string[] = [];
+const typePrinter = createTypePrinter();
+
+rl.on("line", line => {
+  const str = line.trim();
+  if (str.toLowerCase() === "quit" || str.toLowerCase() === "exit") {
+    process.exit();
+  }
+  const idx = str.indexOf(";;");
+  if (idx !== -1) {
+    buf.push(str.slice(0, idx));
+
+    // syntax check
+    const code = buf.join(" ");
+    buf = [];
+    evaluateExpression(code);
+  } else {
+    buf.push(str);
+  }
+  rl.prompt();
+});
+
+function evaluateExpression(code: string) {
+  // syntax check
+  const tree = parse(code);
+
+  if (!tree) {
+    console.log(color.red("Syntax error"));
+    return;
+  }
+
+  // type check
+  const typeResult = getPrimaryType(tree);
+  if (!typeResult.ok) {
+    console.log(color.red("Type error: " + typeResult.value.message));
+    return;
+  }
+
+  const typeStr = typePrinter(typeResult.value.expressionType);
+  process.stdout.write(`${color.yellow("==> ")}${typeStr}: `);
+
+  // evaluation
+  const result = evaluate(tree);
+  if (result.ok) {
+    console.log(result.value);
+  } else {
+    console.log(color.red(result.value.message));
+  }
+}
