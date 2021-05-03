512720cb29923c39363facc165feee5d351e1071
Quramy <yosuke.kurami@gmail.com>
2021-05-04 00:50:05 +0900

    feat: Module builder

---
+++ src/compiler/moduel-builder.ts
@@ -0,0 +1,85 @@
+import { Result, ok, mapValue } from "../structure";
+import { ModuleNode, parse, ModuleBodyNode } from "../wasm";
+
+type LinkFn = (dependent: ModuleNode, self: ModuleNode) => ModuleNode;
+
+const defaultLinkFn: LinkFn = (dependent, self) => {
+  const ret: ModuleNode = {
+    ...self,
+    body: [...self.body, ...dependent.body],
+  };
+  return ret;
+};
+
+export interface ModuleDefinition {
+  readonly name: string;
+  readonly code: string;
+  readonly dependencies?: readonly ModuleDefinition[];
+  readonly link?: LinkFn;
+}
+
+export class ModuleBuilder implements ModuleDefinition {
+  private _name: string;
+  private _code: string;
+  private _dependencies: ModuleDefinition[];
+  private _link?: LinkFn;
+  private additionalFields: ModuleBodyNode[] = [];
+
+  constructor(definition: ModuleDefinition) {
+    this._name = definition.name;
+    this._code = definition.code;
+    this._dependencies = (definition.dependencies ?? []).slice();
+    this._link = definition.link;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get dependencies() {
+    return this._dependencies;
+  }
+
+  addDependency(def: ModuleDefinition) {
+    this._dependencies.push(def);
+    return this;
+  }
+
+  get link() {
+    return this._link;
+  }
+
+  addField(field: ModuleBodyNode) {
+    this.additionalFields.push(field);
+    return this;
+  }
+
+  build(): Result<ModuleNode> {
+    const importedModules = new Set<string>();
+    const createLinkFn = (def: ModuleDefinition): LinkFn => {
+      return (dependent, self) => {
+        if (importedModules.has(def.name)) return dependent;
+        importedModules.add(def.name);
+        if (!def.link) return defaultLinkFn(dependent, self);
+        return def.link(dependent, self);
+      };
+    };
+    const buildInner = (definition: ModuleDefinition): Result<ModuleNode> =>
+      parse(definition.code).mapValue(dependentModule =>
+        (definition.dependencies ?? [])
+          .map(dependencyDefinition =>
+            buildInner(dependencyDefinition).map(mod => ({ mod, linkFn: createLinkFn(dependencyDefinition) })),
+          )
+          .reduce(
+            (mod, dependencyModule) =>
+              mapValue(mod, dependencyModule)((mod, dependency) => ok(dependency.linkFn(mod, dependency.mod))),
+            ok(dependentModule),
+          ),
+      );
+    return buildInner(this).map(mod => ({ ...mod, body: [...mod.body, ...this.additionalFields] }));
+  }
+}
---
+++ src/compiler/module-builder.test.ts
@@ -0,0 +1,95 @@
+import { printAST, factory } from "../wasm";
+import { ModuleBuilder, ModuleDefinition } from "./moduel-builder";
+
+describe(ModuleBuilder, () => {
+  it("should build from simple module definition", () => {
+    const mod = new ModuleBuilder({ name: "main", code: `(module (memory 1))` }).build().unwrap();
+    expect(oneline(printAST(mod))).toBe(`(module (memory 1))`);
+  });
+
+  it("should build from module with dependencies", () => {
+    const dep: ModuleDefinition = {
+      name: "dep",
+      code: `
+        (module
+          (func $fn (result i32)
+            i32.const 0
+          )
+        )
+      `,
+    };
+    const def: ModuleDefinition = {
+      name: "main",
+      code: `
+        (module
+          (func $main (result i32)
+            call $fn
+          )
+        )
+      `,
+      dependencies: [dep],
+    };
+    const mod = new ModuleBuilder(def)
+      .addField(factory.exportNode("main", factory.exportedFunc(factory.identifier("main"))))
+      .build()
+      .unwrap();
+    const expectedSource = `
+      (module
+        (func $fn (result i32)
+          i32.const 0
+        )
+        (func $main (result i32)
+          call $fn
+        )
+        (export "main" (func $main))
+      )
+    `;
+    expect(oneline(printAST(mod))).toBe(oneline(expectedSource));
+  });
+
+  it("should import diamond dependencies once", () => {
+    const dep0: ModuleDefinition = {
+      name: "dep0",
+      code: `
+        (module
+          (memory 1)
+        )
+      `,
+    };
+    const dep1: ModuleDefinition = {
+      name: "dep1",
+      code: `
+        (module
+          (global i32 i32.const 0)
+        )
+      `,
+      dependencies: [dep0],
+    };
+    const def: ModuleDefinition = {
+      name: "main",
+      code: `
+        (module
+          (func $main (result i32)
+            i32.const 0
+          )
+        )
+      `,
+      dependencies: [dep0, dep1],
+    };
+    const mod = new ModuleBuilder(def).build().unwrap();
+    const expectedSource = `
+      (module
+        (memory 1)
+        (global i32 i32.const 0)
+        (func $main (result i32)
+          i32.const 0
+        )
+      )
+    `;
+    expect(oneline(printAST(mod))).toBe(oneline(expectedSource));
+  });
+});
+
+function oneline(text: string) {
+  return text.trim().replace(/\n/g, " ").replace(/\s+/g, " ").replace(/\s*\)/g, ")");
+}
