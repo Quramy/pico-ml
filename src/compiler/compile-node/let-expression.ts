2b5f2d54add2e884e663e0024e0c595ef25f4f39
Quramy <yosuke.kurami@gmail.com>
2021-05-04 17:14:37 +0900

    chore: Modify let expression compiler

--- src/compiler/compile-node/let-expression.ts
+++ src/compiler/compile-node/let-expression.ts
@@ -7,14 +7,16 @@ export const letExpression: CompileNodeFn<"LetExpression"> = (node, ctx, next) =
   ctx.useEnvironment();
   const parentEnv = ctx.getEnv();
   ctx.pushInstruction(getEnvAddrInstr());
-  return next(node.binding, ctx).mapValue(() => {
-    ctx.pushInstruction(newEnvInstr());
-    const childEnv = createChildEnvironment(node.identifier, parentEnv);
-    ctx.setEnv(childEnv);
-    return next(node.exp, ctx).mapValue(() => {
+  return next(node.binding, ctx)
+    .mapValue(() => {
+      ctx.pushInstruction(newEnvInstr());
+      const childEnv = createChildEnvironment(node.identifier, parentEnv);
+      ctx.setEnv(childEnv);
+      return next(node.exp, ctx);
+    })
+    .mapValue(() => {
       ctx.setEnv(parentEnv);
       ctx.pushInstruction(popEnvInstr());
       return ok(true);
     });
-  });
 };
