d5ee98fe2aba3baa2621c79c08c79930b68bea32
Quramy <yosuke.kurami@gmail.com>
2021-05-10 10:09:58 +0900

    chore: Modify editor code

--- playground/src/components/editor/index.tsx
+++ playground/src/components/editor/index.tsx
@@ -1,8 +1,30 @@
+import ace from "ace-builds";
+import "ace-builds/webpack-resolver"; // tell theme, syntax highlight module url to webpack
 import React, { useContext } from "react";
 import cx from "classnames";
-import { setupEditor } from "../../editor";
+
+import { codeContext, CodeContextValue } from "../../context/code-context";
 import styles from "./index.css";
-import { codeContext } from "../../context/code-context";
+
+function setupEditor(element: HTMLElement | null, ctx: CodeContextValue) {
+  if (!element) return;
+  const editor = ace.edit(element, {
+    mode: "ace/mode/ocaml",
+  });
+  editor.setOptions({});
+
+  editor.setTheme("ace/theme/monokai");
+
+  const editSession = editor.getSession();
+  editSession.setTabSize(2);
+
+  editSession.setValue(ctx.initialContent);
+  editor.on("change", () => {
+    ctx.code$.next(editSession.getValue());
+  });
+
+  return editor;
+}
 
 export function Editor() {
   const ctx = useContext(codeContext);
