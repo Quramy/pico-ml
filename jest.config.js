eb5bbd2bef140774cc96c397c79b6fcf3b7432b5
Quramy <yosuke.kurami@gmail.com>
2022-05-17 23:23:39 +0900

    chore: Use npm workspaces

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  globals: {
-    "ts-jest": {
-      diagnostics: false,
-    },
-  },
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-  },
-  setupFilesAfterEnv: ["./.jest/setup.js"],
-  testRegex: "(src/.*\\.test)\\.ts$",
-  testPathIgnorePatterns: ["/node_modules/", "\\.d\\.ts$", "lib/.*", "lib-esm/.*"],
-  watchPathIgnorePatterns: ["playground/.*", "lib/.*", "lib-esm/.*"],
-  moduleFileExtensions: ["js", "ts", "json"],
+  projects: ["<rootDir>/packages/*"],
 };
